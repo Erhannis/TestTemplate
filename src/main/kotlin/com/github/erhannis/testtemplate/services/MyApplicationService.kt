a01bc08f3c4c32225eccfbd17936dc4bbfa394b4
GitHub Action <action@github.com>
2022-06-11 23:10:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/erhannis/testtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/erhannis/testtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.erhannis.testtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.erhannis.testtemplate.MyBundle
 
 class MyApplicationService {
 
